a176cecdfd006a89e823f985b0516800cadb1195
aluemid <62863265+aluemid@users.noreply.github.com>
2021-05-11 14:16:38 +0900

    Initial commit

---
+++ Lesson1/harry.py
@@ -0,0 +1,17 @@
+from logic import *
+
+rain = Symbol("rain")
+hagrid = Symbol("hagrid")
+dumbledore = Symbol("dumbledore")
+
+knowledge = And(
+    Implication(Not(rain), hagrid),
+    Or(hagrid, dumbledore),
+    Not(And(hagrid, dumbledore)),
+    dumbledore
+)
+
+
+
+if __name__ == "__main__":
+    print(model_check(knowledge, rain))
